2cc6088c42d34cd3779b481cd62461f38591e2b3
sangitech <sangitech27@gmail.com>
2024-03-28 13:57:50 +0530

    Initial project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'; 
+import CustomerAdd from './CustomerAdd';
+import CustomerView from './CustomerView';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+function App() {
+  return (
+    <Provider store={store} >
+      <div className="App">
+        <CustomerAdd />
+        <CustomerView />
+      </div>    
+    </Provider>  
+  );
+}
+
+export default App;
---
+++ src/CustomerAdd.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { addCustomer as addCustomerAction } from "./slices/customerSlice";
+import { useDispatch } from "react-redux";
+
+export default function CustomerAdd() {
+    const [input, setInput] = useState("");
+    const dispatch = useDispatch();
+    //const [customers, setCustomers] = useState([]);
+
+    function addCustomer() {
+        if (input) {
+           // setCustomers((previousState) => [...previousState, input]);
+           dispatch(addCustomerAction(input))
+            setInput("");
+        }
+    }
+
+    return (
+        <>
+        <div>
+            <h3>Add Customer</h3>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            <button onClick={addCustomer}>Add</button>
+        </div>        
+       {/*  <CustomerView customers={customers} /> */}
+      </>
+    )
+}
